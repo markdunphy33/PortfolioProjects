8ddd4069023bd3b314215f924719a8aa66e2bef6
markdunphy33 <99384288+markdunphy33@users.noreply.github.com>
2022-02-19 02:29:00 -0800

    Update COVID-19 Data Exploration (GH).sql

--- COVID-19 Data Exploration (GH).sql
+++ COVID-19 Data Exploration (GH).sql
@@ -23,6 +23,7 @@ ORDER BY 1,2
 -- Total Cases vs Total Deaths
 
 -- Shows likelihood of dying if you contract COVID-19 by country (Infection Fatality Rate)
+
 SELECT location, SUM(new_cases) AS Total_Cases,SUM(CAST(new_deaths AS INT)) AS Total_Deaths, SUM(CAST(new_deaths AS INT))/SUM(new_cases)*100 AS IFR
 FROM PortfolioProject..CovidDeaths
 WHERE continent IS NOT NULL
@@ -30,6 +31,7 @@ GROUP BY location
 ORDER BY 4 DESC
 
 -- Shows timeline of Infection Fatality Rate if you contract COVID-19 in your country
+
 SELECT location, FORMAT(date,'yyyy-MM-dd') AS Date, total_cases, total_deaths , (total_deaths / total_cases)*100 AS Daily_IFR
 FROM PortfolioProject..CovidDeaths
 WHERE location LIKE '%states%'
@@ -39,12 +41,14 @@ ORDER BY 1,2
 -- Total Cases vs Population
 
 -- Shows timeline of what percentage of population has had COVID-19
+
 SELECT location, FORMAT(date,'yyyy-MM-dd') AS Date, total_cases, population , (total_cases / population)*100 AS Cases_By_Pop
 FROM PortfolioProject..CovidDeaths
 WHERE location = 'United States'
 ORDER BY 1,2
 
 -- Looking at Countries with highest infection rates compared to Population
+
 SELECT location, MAX(total_cases) as Highest_Infection_Count, population , MAX((total_cases / population))*100 AS Pop_Infection_Percent
 FROM PortfolioProject..CovidDeaths
 GROUP BY location, population
@@ -54,6 +58,7 @@ ORDER BY Pop_Infection_Percent DESC
 -- Total Deaths vs Population
 
 -- Showing Continents with highest death count
+
 SELECT  location, MAX(cast(total_deaths as INT)) as Total_Death_Count
 FROM PortfolioProject..CovidDeaths
 WHERE continent IS NULL 
@@ -63,7 +68,8 @@ AND location <> 'European Union'
 GROUP BY  location
 ORDER BY Total_Death_Count DESC
 
--- Shows % of Population that has died due to COVID
+-- Shows % of Population that has died due to COVID-19
+
 SELECT location, population AS Total_Population, MAX(cast(total_deaths as INT)) as Total_Death_Count, MAX(cast(total_deaths as INT)/ population)*100 AS Percent_Pop_Lost
 FROM PortfolioProject..CovidDeaths
 WHERE continent IS NOT NULL 
@@ -74,6 +80,7 @@ ORDER BY Percent_Pop_Lost DESC
 -- Total Cases vs Vaccinations
 
 -- Shows timeline of new case rate based on vaccinations
+
 SELECT dea.location, FORMAT(dea.date,'yyyy-MM-dd') AS Date, vac.people_fully_vaccinated, dea.new_cases,
 (dea.new_cases / dea.population)*100 AS Case_Rate
 FROM PortfolioProject..CovidDeaths dea
@@ -87,7 +94,8 @@ ORDER BY 1,2
 
 -- Total Vaccinations vs Population
 
--- Shows increase in percentage of population fully vaccinated
+-- Shows increase in percentage of fully vaccinated population
+
 SELECT dea.location, FORMAT(dea.date,'yyyy-MM-dd') AS Date, dea.population, vac.people_fully_vaccinated,(vac.people_fully_vaccinated/dea.population)*100 AS Percent_Pop_FullyVaccinated
 FROM PortfolioProject..CovidDeaths dea
 JOIN PortfolioProject..CovidVaccinations vac
@@ -101,9 +109,10 @@ ORDER BY 1,2
 -- New Cases & New Deaths vs Vaccinations
 
 -- Shows rolling case and death data using PARTITION BY
--- Calculating % of population that contracted COVID and % of population that has passed away due to COVID
+-- Calculating % of population that contracted COVID-19 and % of population that has passed away due to COVID
 
 -- Using CTE to perform above calculation on PARTITION BY data
+
 WITH VACvsCASESvsDEATHS (location, date, population, Fully_Vaccinated, Rolling_Cases, Rolling_Deaths)
 AS
 (
@@ -123,6 +132,7 @@ WHERE location = 'United States'
 ORDER BY 1,2
 
 -- Using Temp Table to perform above calculation on PARTITION BY data
+
 DROP TABLE IF EXISTS VacCaseDeath
 CREATE TABLE VacCaseDeath
 (
@@ -150,7 +160,9 @@ FROM VacCaseDeath
 WHERE location = 'United States'
 ORDER BY 1,2
 
+
 -- Creating view to store later for Tableu visualization
+
 CREATE VIEW RollingPercentData 
 AS
 WITH VACvsCASESvsDEATHS (location, date, population, Fully_Vaccinated, Rolling_Cases, Rolling_Deaths)
@@ -170,6 +182,7 @@ SELECT *,(Rolling_Cases/population)*100 AS Percent_Pop_Contracted, (Rolling_Deat
 FROM VACvsCASESvsDEATHS
 
 -- Accessing and dropping created view
+
 SELECT *
 FROM dbo.RollingPercentData
 
